a5cc10fe132b289c058b67dffafe5d588424b904
ALauraFM <analaurafmendonca@gmail.com>
2023-03-16 19:58:15 -0300

    update

--- Exercise/src/StaticList/List.java
+++ Exercise/src/StaticList/List.java
@@ -1,4 +1,64 @@
 package StaticList;
 
 public class List {
+    private int data[];
+    private int currentSize;
+    private int maxSize;
+
+    //create the list
+    public void SeqList(){
+        maxSize = 100;
+        currentSize = 0;
+        data = new int[maxSize];
+    }
+
+    public boolean Empty(){
+        if (maxSize == 0 ) return true;
+        else return false;
+    }
+
+    public boolean Full(){
+        if (currentSize == maxSize) return true;
+        else return false;
+    }
+
+    public int Size(){
+        return currentSize;
+    }
+
+    public int Element(int pos){
+        int dado;
+
+        if ((pos > currentSize) || (pos <= 0))
+            return -1;
+        dado = data[pos-1];
+        return dado;
+    }
+
+    public boolean Insert (int pos, int dado){
+        if (Full() || (pos > currentSize+1) || (pos<=0)){
+            return false;
+        }
+        for (int i = currentSize; i >= pos; i--){
+            data[i] = data[i-1];
+        }
+        data[pos - 1] = dado;
+        currentSize++;
+        return true;
+    }
+
+    public int Remove(int pos){
+        int dado;
+        if ((pos > currentSize) || (pos < 1 ))
+            return -1;
+        dado = data[pos-1];
+        for (int i = pos - 1; i < currentSize - 1; i++){
+            data[i] = data[i+1];
+        }
+        currentSize--;
+        return dado;
+    }
+
+
+
 }
--- Exercise/src/StaticList/Main.java
+++ Exercise/src/StaticList/Main.java
@@ -1,7 +1,23 @@
 package StaticList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int option;
+
+        List list = new List();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite uma opção: \n1-)Verificar se a lista está vazia" +
+                "\n2-)Verificar se a lista está cheia" +
+                "\n3-)Obter o tamanho da lista" +
+                "\n4-)Obter/modificar o valor do elemento de uma determinada\n" +
+                "posição na lista" +
+                "\n5-)Inserir um elemento em uma determinada posição" +
+                "\n6-)Retirar um elemento de uma determinada posição ");
+
+        
+
     }
 }
